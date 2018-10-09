5119af1924fb7368b7d98f0a6e0bc1a146a1ecba
Jeff Hyatt <jhyatt00@gmail.com>
2018-10-09 18:00:11 -0400

    add AddEntry component

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
 import React from 'react'
-import {StyleSheet, Text, View} from 'react-native'
-import {MaterialCommunityIcons} from '@expo/vector-icons'
+import { View } from 'react-native'
+import AddEntry from './components/AddEntry'
 
 export default class App extends React.Component {
   render() {
-    return (<View style={styles.container}>
-      <MaterialCommunityIcons name='taco' color='orange' size={100}/>
-    </View>)
+    return (
+      <View>
+        <AddEntry />
+      </View>
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-  
-})
