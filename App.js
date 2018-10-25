bf427c99c655c61c7f7e3fbe4d1504da45d0452b
Jeff Hyatt <jhyatt00@gmail.com>
2018-10-25 17:26:41 -0400

    create calendar and related functions

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import History from './components/History'
 
 export default class App extends React.Component {
   store = createStore(reducer)
@@ -13,6 +14,7 @@ export default class App extends React.Component {
       <Provider store={this.store}>
         <View style={{flex: 1}}>
           <AddEntry />
+          <History />
         </View>
       </Provider>
     )
