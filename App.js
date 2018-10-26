826b91118fc5011ac7527944736a61298b63c0d9
Jeff Hyatt <jhyatt00@gmail.com>
2018-10-26 18:44:21 -0400

    add react-navigation and setup tabs

--- App.js
+++ App.js
@@ -1,10 +1,56 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Platform } from 'react-native'
 import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
+import { createMaterialTopTabNavigator, createBottomTabNavigator } from 'react-navigation'
+import { purple, white } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+
+const RouteConfigs = {
+  History: {
+    screen: History,
+    navigationOptions: {
+      tabBarLabel: 'History',
+      tabBarIcon: ({ tintColor }) => <Ionicons name={'ios-bookmarks'} size={30} color={tintColor} />
+    }
+  },
+  AddEntry: {
+    screen: AddEntry,
+    navigationOptions: {
+      tabBarLabel: 'Add Entry',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name={'plus-square'} size={30} color={tintColor} />
+    }
+  }
+}
+
+const TabConfigs = {
+  navigationOptions: {
+    header: null,
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === "ios" ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === "ios" ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+}
+
+
+const Tabs =
+  Platform.OS === 'ios'
+  ? createBottomTabNavigator(RouteConfigs, TabConfigs)
+  : createMaterialTopTabNavigator(RouteConfigs, TabConfigs)
 
 export default class App extends React.Component {
 
@@ -12,7 +58,7 @@ export default class App extends React.Component {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
-          <History />
+          <Tabs />
         </View>
       </Provider>
     )
