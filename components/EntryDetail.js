edff009037691fc53914597e7f1490249610acd0
Jeff Hyatt <jhyatt00@gmail.com>
2018-10-29 11:15:19 -0400

    add date header formatting for entry detail

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
 class EntryDetail extends Component {
+  static navigationOptions = ({ navigation }) => {
+    const {  entryID } = navigation.state.params
+
+    const year = entryID.slice(0, 4)
+    const month = entryID.slice(5, 7)
+    const day = entryID.slice(8)
+
+    return {
+      title: `${month}/${day}/${year}`
+    }
+  }
   render() {
     return (
       <View>
